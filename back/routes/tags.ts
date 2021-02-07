dbd3acada7d0371b38dd8b8a06d19ae829a3c944
VadimTaranovscky <43213486+VadimTaranovscky@users.noreply.github.com>
2021-02-07 16:58:41 +0400

    summary

--- back/routes/tags.ts
+++
@@ -1,9 +0,0 @@
-import * as express from "express";
-
-import { tagController } from "../controllers/TagController";
-
-const router = express.Router();
-
-router.get("/", tagController.index);
-
-export const tagsRouter = router;
--- back/utils/getMD5Hash.ts
+++
@@ -1,4 +0,0 @@
-import { createHash } from "crypto";
-
-export const generateMD5Hash = (value: string) =>
-  createHash("md5").update(value).digest("hex");
